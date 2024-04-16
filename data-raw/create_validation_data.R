b52d90b26214c9ccf099f8ec638ca39624add39f
sempwn <mike.irvine@bccdc.ca>
2024-04-16 16:41:17 -0700

    Update script to add data to package.

--- data-raw/create_validation_data.R
+++ data-raw/create_validation_data.R
@@ -34,7 +34,7 @@ plot_data <- function(d){
 experiments <- expand_grid(sigma = seq(0.05,0.5,by=0.05),
                            zeta = seq(0.05, 0.5, by=0.05))
 
-results <- tibble()
+experimental_validation_data <- tibble()
 
 pb <- progress_bar$new(
   format = "  running [:bar] :percent eta: :eta",
@@ -65,72 +65,10 @@ for(i in 1:nrow(experiments)){
     ) %>%
     mutate(experiment = i)
 
-  results <- results %>% bind_rows(row_results)
+  experimental_validation_data <- experimental_validation_data %>%
+    bind_rows(row_results)
   pb$tick()
 }
 
-results %>% saveRDS(
-  file = here(
-    "revised_manuscript", "plots",
-    "comprehensive_simulation_results.rds"
-  )
-)
+experimental_validation_data %>% usethis::use_data()
 
-
-###############################
-## Plot of resulting figures ##
-###############################
-results <- readRDS(
-  file = here(
-    "revised_manuscript", "plots",
-    "comprehensive_simulation_results.rds"
-  )
-)
-# plot for sigma
-
-p_res_sigma <- results %>%
-  group_by(experiment) %>%
-  mutate(zeta_val = sum(true_value * as.numeric(.variable == "zeta"))) %>%
-  ungroup() %>%
-  filter(.variable == "sigma") %>%
-  ggplot(aes(x=true_value,y=true_value)) +
-  geom_abline(intercept = 0, slope = 1, linetype = "dashed", color = "gray20") +
-  geom_errorbar(aes(y = p50,ymin=p05,ymax=p95, color = zeta_val,
-                    group = experiment)) +
-  geom_point(aes(y = p50, color = zeta_val)) +
-  theme_classic() +
-  labs(color = parse(text = TeX("$\\zeta$")),
-       x = "True value", y = "Inferred value")
-
-
-show(p_res_sigma)
-
-p_res_sigma %>% saveRDS(
-  file = here(
-    "revised_manuscript", "plots",
-    "comprehensive_simulation_plot_sigma.rds"
-  )
-)
-
-p_res_zeta <- results %>%
-  group_by(experiment) %>%
-  mutate(sigma_val = sum(true_value * as.numeric(.variable == "sigma"))) %>%
-  ungroup() %>%
-  filter(.variable == "zeta") %>%
-  ggplot(aes(x=true_value,y=true_value)) +
-  geom_abline(intercept = 0, slope = 1, linetype = "dashed", color = "gray20") +
-  geom_errorbar(aes(y = p50,ymin=p05,ymax=p95, color = sigma_val,
-                    group = experiment)) +
-  geom_point(aes(y = p50, color = sigma_val)) +
-  theme_classic() +
-  labs(color = parse(text = TeX("$\\sigma$")),
-       x = "True value", y = "Inferred value")
-
-show(p_res_zeta)
-
-p_res_zeta %>% saveRDS(
-  file = here(
-    "revised_manuscript", "plots",
-    "comprehensive_simulation_plot_zeta.rds"
-  )
-)
